7c49a8556ef7849fab151b462c91a703ce15de59
WonHeeSoo <wonhs717@gmail.com>
2020-09-01 15:09:00 +0900

    [Update] Setting linked list

--- PracticeAlgorithm/PracticeAlgorithm/Board.cs
+++ PracticeAlgorithm/PracticeAlgorithm/Board.cs
@@ -42,22 +42,75 @@ namespace PracticeAlgorithm
             Count--;
         }
     }
+
+    class MyLinkedListNode<T>
+    {
+        public T Data;
+        public MyLinkedListNode<T> Next;
+        public MyLinkedListNode<T> Prev;
+    }
+
+    class MyLinkedList<T>
+    {
+        public MyLinkedListNode<T> Head = null; // 첫번째
+        public MyLinkedListNode<T> Tail = null; // 마지막
+        public int Count = 0;
+
+        public MyLinkedListNode<T> AddLast(T data)
+        {
+            MyLinkedListNode<T> newNode = new MyLinkedListNode<T>();
+            newNode.Data = data;
+
+            // 만약에 아직 노드가 존재하지 않는다면, 새로 추가한 첫번째 노드가 곧 Head이다.
+            if (Head == null)
+                Head = newNode;
+
+            // 기존의 [마지막 노드]와 [새로 추가되는 노드]를 연결해준다.
+            if (Tail != null)
+            {
+                Tail.Next = newNode;
+                newNode.Prev = Tail;
+            }
+
+            // [새로 추가되는 방]을 [마지막 방]으로 인정한다.
+            Tail = newNode;
+            Count++;
+            return newNode;
+        }
+
+        public void Remove(MyLinkedListNode<T> node)
+        {
+            // [기존의 첫번째 노드의 다음 노드]를 [첫번째 노드]로 인정한다.
+            if (Head == node)
+                Head = Head.Next;
+
+            // [기존의 마지막 노드의 이전 노드]를 [마지막 노드]로 인정한다.
+            if (Tail == node)
+                Tail = Tail.Prev;
+
+            if (node.Prev != null)
+                node.Prev.Next = node.Next;
+
+            if (node.Next != null)
+                node.Next.Prev = node.Prev;
+
+            Count--;
+        }
+    }
     class Board
     {
         public int[] _data = new int[25]; // 배열
         public MyList<int> _data2 = new MyList<int>(); // 동적 배열
-        public LinkedList<int> _data3 = new LinkedList<int>(); // 연결 리스트
+        public MyLinkedList<int> _data3 = new MyLinkedList<int>(); // 연결 리스트
         public void Initalize()
         {
-            _data2.Add(101);
-            _data2.Add(102);
-            _data2.Add(103);
-            _data2.Add(104);
-            _data2.Add(105);
+            _data3.AddLast(101);
+            _data3.AddLast(102);
+            MyLinkedListNode<int> node = _data3.AddLast(103);
+            _data3.AddLast(104);
+            _data3.AddLast(105);
 
-            int temp = _data2[2];
-
-            _data2.RemoveAt(2);
+            _data3.Remove(node);
         }
     }
 }
