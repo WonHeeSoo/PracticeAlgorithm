7fcb3e39587f3b1d92656f1ecbd5f6e45beb6047
WonHeeSoo <wonhs717@gmail.com>
2020-09-01 15:26:19 +0900

    [Update] Init map

--- PracticeAlgorithm/PracticeAlgorithm/Board.cs
+++ PracticeAlgorithm/PracticeAlgorithm/Board.cs
@@ -99,18 +99,62 @@ namespace PracticeAlgorithm
     }
     class Board
     {
-        public int[] _data = new int[25]; // 배열
-        public MyList<int> _data2 = new MyList<int>(); // 동적 배열
-        public MyLinkedList<int> _data3 = new MyLinkedList<int>(); // 연결 리스트
-        public void Initalize()
-        {
-            _data3.AddLast(101);
-            _data3.AddLast(102);
-            MyLinkedListNode<int> node = _data3.AddLast(103);
-            _data3.AddLast(104);
-            _data3.AddLast(105);
+        const char CIRCLE = '\u25cf';
+        public TileType[,] _tile; // 배열
+        public int _size;
 
-            _data3.Remove(node);
+        public enum TileType
+        {
+            Empty,
+            Wall,
+        }
+
+        public void Initialize(int size)
+        {
+            _tile = new TileType[size, size];
+            _size = size;
+
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x == 0 || x == _size - 1 || y == 0 || y == size - 1)
+                        _tile[y, x] = TileType.Wall;
+                    else
+                    {
+                        _tile[y, x] = TileType.Empty;
+                    }
+                }
+            }
+        }
+
+        public void Render()
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    Console.ForegroundColor = GetTileColor(_tile[y, x]);
+
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = prevColor;
+        }
+
+        ConsoleColor GetTileColor(TileType type)
+        {
+            switch (type)
+            {
+                case TileType.Empty:
+                    return ConsoleColor.Green;
+                case TileType.Wall:
+                    return ConsoleColor.Red;
+                default:
+                    return ConsoleColor.Green;
+            }
         }
     }
 }
--- PracticeAlgorithm/PracticeAlgorithm/Program.cs
+++ PracticeAlgorithm/PracticeAlgorithm/Program.cs
@@ -7,12 +7,11 @@ namespace PracticeAlgorithm
         static void Main(string[] args)
         {
             Board board = new Board();
-            board.Initalize();
+            board.Initialize(25);
 
             Console.CursorVisible = false; // 커서 보이지 않게 하기
 
             const int WAIT_TICK = 1000 / 30;
-            const char CIRCLE = '\u25cf';
 
             int lastTick = 0;
             while (true)
@@ -33,16 +32,7 @@ namespace PracticeAlgorithm
 
                 // 렌더링
                 Console.SetCursorPosition(0, 0); // 커서 위치
-
-                for (int i = 0; i < 25; i++)
-                {
-                    for (int j = 0; j < 25; j++)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write(CIRCLE);
-                    }
-                    Console.WriteLine();
-                }
+                board.Render();
             }
         }
     }
