3378192e55bafcdce1a43dc17bc0ea9581366fbb
WonHeeSoo <wonhs717@gmail.com>
2020-09-03 16:29:21 +0900

    [Update] Create DFS algorithm

--- PracticeAlgorithm/Exercise/Program.cs
+++ PracticeAlgorithm/Exercise/Program.cs
@@ -24,6 +24,45 @@ namespace Exercise
             new List<int>() { 3, 5 },
             new List<int>() { 4 },
         };
+
+        bool[] visited = new bool[6];
+        // 1) 우선 now 부터 방분하고,
+        // 2) now와 연결된 정점들을 하나씩 확인해서, 아직 미발견(미방문) 상태라면 방문한다.
+        public void DFS(int now) // int[,] adj = new int[6, 6]를 사용한 방법
+        {
+            Console.WriteLine(now);
+            visited[now] = true; // 1) 우선 now부터 방문하고
+            
+            for (int next = 0; next < 6; next++)
+            {
+                if (adj[now, next] == 0) // 연결되어 있지 않으면 스킵.
+                    continue;
+                if (visited[next]) // 이미 방문했으면 스킵.
+                    continue;
+                DFS(next);
+            }
+        }
+
+        public void DFS2(int now) // List<int>[] adj2 = new List<int>[]를 사용한 방법
+        {
+            Console.WriteLine(now);
+            visited[now] = true; // 1) 우선 now부터 방문하고
+
+            foreach (int next in adj2[now])
+            {
+                if (visited[next]) // 이미 방문했다면 스킵.
+                    continue;
+                DFS2(next);
+            }
+        }
+
+        public void SearchAll()
+        {
+            visited = new bool[6];
+            for (int now = 0; now < 6; now++)
+                if (visited[now] == false)
+                    DFS(now);
+        }
     }
     class Program
     {
@@ -31,6 +70,8 @@ namespace Exercise
         {
             // DFS (Depth First Search 깊이 우선 탐색)
             // BFS (Breadth First Search 너비 우선 탐색)
+            Graph graph = new Graph();
+            graph.DFS(1);
         }
     }
 }
