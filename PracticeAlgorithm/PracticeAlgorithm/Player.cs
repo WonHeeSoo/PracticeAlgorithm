6210d690d5c5f0aa0151084b7f46704e4a449ed7
WonHeeSoo <wonhs717@gmail.com>
2020-09-04 14:33:57 +0900

    [Add] Create BFS maze rule

--- PracticeAlgorithm/PracticeAlgorithm/Player.cs
+++ PracticeAlgorithm/PracticeAlgorithm/Player.cs
@@ -33,6 +33,58 @@ namespace PracticeAlgorithm
             PosY = posY;
             _board = board;
 
+            BFS();
+        }
+
+        void BFS()
+        {
+            int[] deltaY = new int[] { -1, 0, 1, 0 };
+            int[] deltaX = new int[] { 0, -1, 0, 1 };
+
+            bool[,] found = new bool[_board.Size, _board.Size];
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
+
+            Queue<Pos> q = new Queue<Pos>();
+            q.Enqueue(new Pos(PosY, PosX));
+            found[PosY, PosX] = true;
+            parent[PosY, PosX] = new Pos(PosY, PosX);
+
+            while (q.Count > 0)
+            {
+                Pos pos = q.Dequeue();
+                int nowY = pos.Y;
+                int nowX = pos.X;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    int nextY = nowY + deltaY[i];
+                    int nextX = nowX + deltaX[i];
+                    if (nextX < 0 || nextX >= _board.Size || nextY < 0 || nextY >= _board.Size)
+                    if (_board.Tile[nextY, nextX] == Board.TileType.Wall)
+                        continue;
+                    if (found[nextY, nextX])
+                        continue;
+
+                    q.Enqueue(new Pos(nextY, nextX));
+                    found[nextY, nextX] = true; 
+                }
+            }
+
+            int y = _board.DestY;
+            int x = _board.DestX;
+            while (parent[y, x].Y != y || parent[y, x].X != x)
+            {
+                _points.Add(new Pos(y, x));
+                Pos pos = parent[y, x];
+                y = pos.Y;
+                x = pos.X;
+            }
+            _points.Add(new Pos(y, x));
+            _points.Reverse(); // 목표를 찾고나면 다시 돌아가기 때문에 뒤집어서 움직이지 않게 함
+        }
+
+        void RightHand()
+        {
             // 현재 바라보고 있는 방향을 기준으로, 좌표 변화를 나타낸다
             int[] frontY = new int[] { -1, 0, 1, 0 };
             int[] frontX = new int[] { 0, -1, 0, 1 };
@@ -41,7 +93,7 @@ namespace PracticeAlgorithm
 
             _points.Add(new Pos(PosY, PosX));
             // 목적지에 도착하기 전에는 계속 실행
-            while (PosY != board.DestY || PosX != board.DestX)
+            while (PosY != _board.DestY || PosX != _board.DestX)
             {
                 // 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인.
                 if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
@@ -68,6 +120,7 @@ namespace PracticeAlgorithm
                 }
             }
         }
+
         const int MOVE_TICK = 10;
         int _sumTick = 0;
         int _lastIndex = 0;
